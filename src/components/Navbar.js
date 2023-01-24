4fdb1e6c1f9d1d6ed501eb6408efbe452ae45dbe
lewonid <45365975+lewonid@users.noreply.github.com>
2023-01-24 02:16:18 +0200

    minor css fixes, implemented Routes, navbar final version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Home from '../pages/Home'
 import '../components/style.css'
 import logo from '../assets/images/logo-v1.png'
 import { RxHamburgerMenu } from 'react-icons/rx'
 import { AiOutlineClose } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import ExerciseDetail from '../pages/ExerciseDetail'
 
 const Navbar = () => {
   let mobile = false
@@ -31,8 +34,10 @@ const Navbar = () => {
         <img src={logo} alt='gym logo'></img>
         <div className='NavLinks'>
           <h1 onClick={mobileMenu} className='HamburgerMenuMobile'><RxHamburgerMenu /></h1>
-          <h3 className='navLink'>Home<div className='active'></div></h3>
-          <h3 className='navLink'>Exercises</h3>
+          {/* <h3 className='navLink'>Home<div className='active'></div></h3>
+          <h3 className='navLink'>Exercises</h3> */}
+          <Link style={{textDecoration: 'none'}} to="/" element={<Home />}><h3 className='navLink'>Home<div className='active'></div></h3> </Link>
+          <Link style={{textDecoration: 'none'}} to="/exercises" element={<ExerciseDetail />}><h3 className='navLink'>Exercises</h3></Link>
           <h3 className='navLink'>Contact</h3>
         </div>
       </div>
