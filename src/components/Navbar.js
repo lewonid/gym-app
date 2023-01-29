7d02b6e2f27612193cff2bb14108e2856e5441b1
lewonid <45365975+lewonid@users.noreply.github.com>
2023-01-29 05:05:40 +0200

    final landing page version, +footer, +responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Home from '../pages/Home'
-import '../components/style.css'
-import logo from '../assets/images/logo-v1.png'
+import logo from '../assets/images/logo-v4.png'
 import { RxHamburgerMenu } from 'react-icons/rx'
 import { AiOutlineClose } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import ExerciseDetail from '../pages/ExerciseDetail'
+import Contact from '../pages/Contact'
 
 const Navbar = () => {
   let mobile = false
@@ -21,14 +21,20 @@ const Navbar = () => {
       mobile = false;
     }
   }
+  const buttonClicked = () => {
+    const element = document.getElementById('mobile');
+    element.classList.remove('active-menu');
+    mobile = false;
+  }
 
   return (
     <>
       <div className='Mobile-Menu-Buttons' id='mobile'>
         <h2 onClick={mobileMenu} className='mobile-close-button'><AiOutlineClose /></h2>
-        <h3>Home</h3>
-        <h3>Exercises</h3>
-        <h3>Contact</h3>
+        {/* <h3>Home</h3> */}
+        <Link onClick={buttonClicked} className='mobile-button' to="/" element={<Home />}><h3>Home</h3></Link>
+        <Link onClick={buttonClicked} className='mobile-button' to="/exercises" element={<ExerciseDetail />}><h3>Exercises</h3></Link>        
+        <Link onClick={buttonClicked} className='mobile-button' to="/contact" element={<Contact />}><h3>Contact</h3></Link>  
       </div>
       <div className='Navbar'>
         <img src={logo} alt='gym logo'></img>
