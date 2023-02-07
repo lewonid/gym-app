3d9ae4c98cc98d0973f35813b3fed4a2f2bd1055
unknown <leeonard_21@yahoo.com>
2023-02-07 02:38:44 +0200

    implemented RapidAPI and ScrollToTop when browsing between pages

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { fetchData, exerciseOptions } from '../utils/fetchData'
 
 const ExerciseDetail = () => {
+
+  const SearchExercises = () => {
+    const [search, setSearch] = useState('');
+
+    const handleSearch = async () => {
+      if (search) {
+        const exerciseData = await fetchData('https://exerciseapi3.p.rapidapi.com/search/muscles/', exerciseOptions);
+      }
+      console.log(SearchExercises())
+    }
+  }
+
   return (
-    <div>ExerciseDetail here soon.</div>
+    <div className='ExerciseDetailWrapper'>
+      <h1 onClick={SearchExercises}>Clickme</h1>
+    </div>
   )
 }
 
