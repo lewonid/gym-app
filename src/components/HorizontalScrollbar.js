9188cc7e8b06987f707f6bbe83baef3145fb7b14
lewonid <45365975+lewonid@users.noreply.github.com>
2023-02-19 18:42:12 +0200

    .

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { CgGym } from 'react-icons/cg'
+import { BsArrowLeft, BsArrowRight} from 'react-icons/bs'
 
-const HorizontalScrollbar = ({ data }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+
+  const ref = useRef(null);
+  const scroll = (scrollOffset) =>{
+    ref.current.scrollLeft += scrollOffset;
+  };
+
   return (
-    <div>
+    <>
+    <div ref={ref} className='bodyParts'>
         {data.map((item) => (
             <div className='bodyPart'
                 key={item.id || item}
                 itemID={item.id || item}
                 title={item.id || item}
-            >
-                {item}
+                sx={
+                  bodyPart === item ? {
+                    borderTop: '4px solid #ff2625',
+                    backgroundColor: '#fff'
+                } : ''
+              }
+            >   <CgGym id='bodyIcon'/>
+                {item} {bodyPart} {setBodyPart}
             </div>
         )
         )}
     </div>
+    <div className='HorizontalScrollButtons'>
+      <BsArrowLeft onClick={() => scroll(-200)} id='arrow'/>
+      <BsArrowRight onClick={() => scroll(200)} id='arrow' />
+    </div>
+    </>
   )
 }
 
