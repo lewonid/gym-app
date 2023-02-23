a2937df0cbd7056399aa3a5e85de09ee2913ae10
unknown <leeonard_21@yahoo.com>
2023-02-24 00:44:08 +0200

    .

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,38 +1,33 @@
 import React, { useRef } from 'react'
 import { CgGym } from 'react-icons/cg'
-import { BsArrowLeft, BsArrowRight} from 'react-icons/bs'
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
 
   const ref = useRef(null);
-  const scroll = (scrollOffset) =>{
+  const scroll = (scrollOffset) => {
     ref.current.scrollLeft += scrollOffset;
   };
-
+  console.log(bodyPart)
   return (
     <>
-    <div ref={ref} className='bodyParts'>
+      <div ref={ref} className='bodyParts'>
         {data.map((item) => (
-            <div className='bodyPart'
-                key={item.id || item}
-                itemID={item.id || item}
-                title={item.id || item}
-                sx={
-                  bodyPart === item ? {
-                    borderTop: '4px solid #ff2625',
-                    backgroundColor: '#fff'
-                } : ''
-              }
-            >   <CgGym id='bodyIcon'/>
-                {item} {bodyPart} {setBodyPart}
-            </div>
+          <div onClick={event => setBodyPart(item)} className='bodyPart'
+            key={item.id || item}
+            itemID={item.id || item}
+            title={item.id || item}
+            style={bodyPart === item ? { borderTop: '4px solid #0087ca' } : {}}
+          >   <CgGym id='bodyIcon' />
+            {item}
+          </div>
         )
         )}
-    </div>
-    <div className='HorizontalScrollButtons'>
-      <BsArrowLeft onClick={() => scroll(-200)} id='arrow'/>
-      <BsArrowRight onClick={() => scroll(200)} id='arrow' />
-    </div>
+      </div>
+      <div className='HorizontalScrollButtons'>
+        <BsArrowLeft onClick={() => scroll(-200)} id='arrow' />
+        <BsArrowRight onClick={() => scroll(200)} id='arrow' />
+      </div>
     </>
   )
 }
