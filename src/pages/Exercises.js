8cb6948a8ea1ae53fd3558c27d807f0e885c8f2b
unknown <leeonard_21@yahoo.com>
2023-02-24 00:44:08 +0200

    .

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -18,8 +18,10 @@ const Exercises = () => {
 
     fetchExercisesData();
   }, [])
-  
 
+  const handleBodyPart = (bodyPart) => {
+    setBodyPart(bodyPart);
+  };
   const handleSearch = async () => {
     if (search) {
       // const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
@@ -27,9 +29,9 @@ const Exercises = () => {
 
       const searchedExercises = exercisesData.filter(
         (exercise) => exercise.name.toLowerCase().includes(search)
-        || exercise.target.toLowerCase().includes(search)
-        || exercise.equipment.toLowerCase().includes(search)
-        || exercise.bodyPart.toLowerCase().includes(search)
+          || exercise.target.toLowerCase().includes(search)
+          || exercise.equipment.toLowerCase().includes(search)
+          || exercise.bodyPart.toLowerCase().includes(search)
       )
       setSearch('');
       setExercises(searchedExercises);
@@ -43,7 +45,7 @@ const Exercises = () => {
           <div className='h-line'></div>
           <h1>Find your favorite exercises</h1>
           <p>Here you can find the most important exercises from our gym.<br></br>Regular physical activity can improve your muscle strength and boost your endurance. Exercise delivers oxygen and nutrients to your tissues and helps your cardiovascular system work more efficiently. And when your heart and lung health improve, you have more energy to tackle daily chores.
-            </p>
+          </p>
           <p>Remember to check with your doctor before starting a new exercise program, especially if you have any concerns about your fitness, haven't exercised for a long time, have chronic health problems, such as heart disease, diabetes or arthritis.</p>
           <p>Train better untill you are the best!</p>
           <p>Train better and be the best version of yourself!</p>
@@ -52,17 +54,17 @@ const Exercises = () => {
       </div>
       <div className='ExercisesContent'>
         <form id='form'>
-            <input
+          <input
             type="text"
             value={search}
             placeholder="Search Exercises"
             onChange={e => setSearch(e.target.value.toLowerCase())}
-            />
-            <button onClick={handleSearch} type='button'>
+          />
+          <button onClick={handleSearch} type='button'>
             Search
           </button>
-          </form>
-        <HorizontalScrollbar data = { bodyParts } bodyPart = {bodyPart} setBodyPart={setBodyPart} />
+        </form>
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       </div>
     </div>
   )
